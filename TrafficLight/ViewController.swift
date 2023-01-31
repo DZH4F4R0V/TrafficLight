90391fdd05367a9575a30cb9d530b5778ea41181
DZH4F4R0V <jeff-122@ya.ru>
2023-01-31 22:54:57 +0300

    Adjustable Traffic Light

--- TrafficLight/ViewController.swift
+++ TrafficLight/ViewController.swift
@@ -9,11 +9,41 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var stackView: UIStackView!
+    @IBOutlet var toggleButton: UIButton!
+    @IBOutlet var redView: UIView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        stackView.isHidden.toggle()
+        toggleButton.configuration = setupButton(with: "Turn on Traffic Light")
+        redView.layer.cornerRadius = 0.5
     }
 
-
+    @IBAction func toggleButtonDidTapped() {
+        toggleButton.isHidden.toggle()
+        toggleButton.configuration = setupButton(
+            with: toggleButton.isHidden ?
+            "Turn on Traffic Light" :
+                "Change Light"
+        )
+    }
+    
+    
+    private func setupButton(with title: String) ->
+        UIButton.Configuration {
+        var buttonConfiguration = UIButton.Configuration.filled()
+        buttonConfiguration.title = title
+        buttonConfiguration.baseBackgroundColor = #colorLiteral(red: 0.6711818576, green: 0.124547638, blue: 0.526468575, alpha: 1)
+        buttonConfiguration.buttonSize = .large
+        buttonConfiguration.cornerStyle = .large
+        buttonConfiguration.attributedTitle?.font = UIFont.systemFont(ofSize: 24)
+        
+        return buttonConfiguration
+    }
+    
+    private func setupView(with title: String) ->
+    UIView
+    
 }
 
