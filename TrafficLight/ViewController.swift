1d112e181190665511d5cad73df9d38e92c87fe0
DZH4F4R0V <jeff-122@ya.ru>
2023-01-31 23:56:15 +0300

    Half done work

--- TrafficLight/ViewController.swift
+++ TrafficLight/ViewController.swift
@@ -9,24 +9,39 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    @IBOutlet var stackView: UIStackView!
     @IBOutlet var toggleButton: UIButton!
     @IBOutlet var redView: UIView!
+    @IBOutlet var yellowView: UIView!
+    @IBOutlet var greenView: UIView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        stackView.isHidden.toggle()
-        toggleButton.configuration = setupButton(with: "Turn on Traffic Light")
-        redView.layer.cornerRadius = 0.5
+        toggleButton.configuration = setupButton(with: "START")
+        
+        redView.layer.cornerRadius = 85
+        yellowView.layer.cornerRadius = 85
+        greenView.layer.cornerRadius = 85
+
+        redView.alpha = 0.3
+        yellowView.alpha = 0.3
+        greenView .alpha = 0.3
     }
 
     @IBAction func toggleButtonDidTapped() {
-        toggleButton.isHidden.toggle()
         toggleButton.configuration = setupButton(
             with: toggleButton.isHidden ?
-            "Turn on Traffic Light" :
-                "Change Light"
+            "START" :
+                "NEXT"
         )
+        if redView.alpha != 1 {
+            redView.alpha = 1
+        } else if yellowView.alpha != 1 {
+            redView.alpha = 0.3
+            yellowView.alpha = 1
+        } else {
+            yellowView.alpha = 0.3
+            greenView.alpha = 1
+        }
     }
     
     
@@ -34,16 +49,12 @@ class ViewController: UIViewController {
         UIButton.Configuration {
         var buttonConfiguration = UIButton.Configuration.filled()
         buttonConfiguration.title = title
-        buttonConfiguration.baseBackgroundColor = #colorLiteral(red: 0.6711818576, green: 0.124547638, blue: 0.526468575, alpha: 1)
+        buttonConfiguration.baseBackgroundColor = #colorLiteral(red: 0, green: 0, blue: 0, alpha: 1)
         buttonConfiguration.buttonSize = .large
         buttonConfiguration.cornerStyle = .large
         buttonConfiguration.attributedTitle?.font = UIFont.systemFont(ofSize: 24)
-        
         return buttonConfiguration
     }
-    
-    private func setupView(with title: String) ->
-    UIView
     
 }
 
